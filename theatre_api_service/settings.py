0a59912067abe06406e06fa93ae5d5c7a281ed7f
Sergii Semenets <serhey.semenets@gmail.com>
2024-08-10 21:09:09 +0300

    add SPECTACULAR_SETTINGS in settings project

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -164,3 +164,16 @@ CACHES = {
         "LOCATION": "unique-snowflake",
     }
 }
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Theatre Service API",
+    "DESCRIPTION": "Api for theatre service",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    }
+}
