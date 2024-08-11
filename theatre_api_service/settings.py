6dc07aa3b39261f0f71d75ef72f1d63137902ef4
Sergii Semenets <serhey.semenets@gmail.com>
2024-08-11 23:22:12 +0300

    add load_dotenv() in settings.py

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = \
-    "django-insecure-%e11tb83$**)g-!u=n(%hw&#$zuyef+-#nw=-yb*zq2n__vk(!"
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
